64efdd6866d0e5b12150d486d09e464f71cd3de6
Raparker88 <raparker88@gmail.com>
2026-01-09 13:55:28 -0600

    adding instance id

--- rockapi/views/health_check.py
+++ rockapi/views/health_check.py
@@ -1,4 +1,17 @@
 from django.http import JsonResponse
+import requests
 
 def health_check(request):
-    return JsonResponse({"status": "ok"})
+    return JsonResponse({
+        "status": "ok",
+        "instance": get_instance_id()
+    })
+
+def get_instance_id():
+    try:
+        return requests.get(
+            "http://169.254.169.254/latest/meta-data/instance-id",
+            timeout=0.2
+        ).text
+    except:
+        return "unknown"
